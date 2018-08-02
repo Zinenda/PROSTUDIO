42cab6349f205355794b32d2be44a1c90faeb272
Ivo Zinenda <izinenda@gmail.com>
2018-08-02 23:55:54 +0200

    nav system

---
+++ assets/js/nav.js
@@ -0,0 +1,9 @@
+/* Open when someone clicks on the span element */
+function openNav() {
+    document.getElementsByClassName("myNav").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+    document.getElementsByClassName("myNav").style.width = "0%";
+}
