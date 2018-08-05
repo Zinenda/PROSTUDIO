07ab8929d8d2bc7ec95cd6359feb5c449fbec659
Ivo Zinenda <izinenda@gmail.com>
2018-08-05 21:24:18 +0200

    nav code working

--- assets/js/nav.js
+++
@@ -1,9 +0,0 @@
-/* Open when someone clicks on the span element */
-function openNav() {
-    document.getElementsByClassName("myNav").style.width = "100%";
-}
-
-/* Close when someone clicks on the "x" symbol inside the overlay */
-function closeNav() {
-    document.getElementsByClassName("myNav").style.width = "0%";
-}
