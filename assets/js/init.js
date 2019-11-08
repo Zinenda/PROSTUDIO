7b73e2efbf96277c53c31f05820193671da8f866
Ivo Zinenda <izinenda@gmail.com>
2019-11-08 10:36:26 +0200

    redesign da front page

---
+++ assets/js/init.js
@@ -0,0 +1,79 @@
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+    $('.scrollspy').scrollSpy();
+    $('.tooltipped').tooltip();
+    $('.fixed-action-btn').floatingActionButton();
+    $('.modal').modal();
+    $('.materialboxed').materialbox();
+
+    $('.slick-servicos').slick({
+	dots: true,
+	infinite: false,
+	speed: 300,
+	slidesToShow: 4,
+	slidesToScroll: 4,
+	responsive: [
+		{
+		breakpoint: 1024,
+		settings: {
+			slidesToShow: 3,
+			slidesToScroll: 3,
+			infinite: true,
+			dots: true
+		}
+		},
+		{
+		breakpoint: 600,
+		settings: {
+			slidesToShow: 2,
+			slidesToScroll: 2
+		}
+		},
+		{
+		breakpoint: 480,
+		settings: {
+			slidesToShow: 1,
+			slidesToScroll: 1
+		}
+		}
+	]
+	});
+
+	$('.slick-clientes').slick({
+		dots: false,
+		lazyLoad: 'ondemand',
+		infinite: true,
+		autoplay: true,
+		autoplaySpeed: 2000,
+		slidesToShow: 6,
+		slidesToScroll: 3,
+		responsive: [
+			{
+			breakpoint: 1024,
+			settings: {
+				slidesToShow: 5,
+				slidesToScroll: 3,
+				infinite: true,
+				dots: true
+			}
+			},
+			{
+			breakpoint: 600,
+			settings: {
+				slidesToShow: 4,
+				slidesToScroll: 2
+			}
+			},
+			{
+			breakpoint: 480,
+			settings: {
+				slidesToShow: 3,
+				slidesToScroll: 1
+			}
+			}
+			// You can unslick at a given breakpoint now by adding:
+			// settings: "unslick"
+			// instead of a settings object
+		]
+		});
+});
--- collections/_galeria/corredor-do-hotel-cardoso.md
+++
@@ -1,5 +0,0 @@
----
-title: Corredor do Hotel Cardoso
-photo: 'https://ucarecdn.com/1ef8264e-6829-48b1-ad3c-7424777d6a39/'
----
-
